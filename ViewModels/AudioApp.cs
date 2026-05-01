8d9240ea6d5522cafe3829aa95e2bd533a3a5b64
Vlad <r3ptil4@protonmail.com>
2026-05-01 20:35:09 +0300

    Implemented audio hooks to make the sliders work

--- ViewModels/AudioApp.cs
+++ ViewModels/AudioApp.cs
@@ -1,8 +1,59 @@
-﻿namespace AudioNode.ViewModels
+﻿using NAudio.CoreAudioApi;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AudioNode.ViewModels
 {
-    public class AudioApp
+    public class AudioApp : INotifyPropertyChanged
     {
+        private AudioSessionControl _session;
+        private SimpleAudioVolume _volumeControl;
+
         public string AppName { get; set; } = string.Empty;
-        public double Volume { get; set; } // 0.0 to 100.0
+
+        public AudioApp(AudioSessionControl session)
+        {
+            _session = session;
+            _volumeControl = session.SimpleAudioVolume;
+            AppName = GetProcessName(session.GetProcessID);
+        }
+
+        public string VolumeText => $"{(int)Volume}%";
+        public double Volume
+        {
+            get => _volumeControl.Volume * 100;
+            set
+            {
+                _volumeControl.Volume = (float)(value / 100);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(VolumeText));
+            }
+        }
+
+        // Helper to turn the Process ID into a readable name
+        private string GetProcessName(uint processId)
+        {
+            if (processId == 0) return "System Sounds";
+
+            try
+            {
+                var process = Process.GetProcessById((int)processId);
+                // MainWindowTitle gets "Spotify Premium", ProcessName gets "Spotify"
+                return !string.IsNullOrWhiteSpace(process.MainWindowTitle)
+                    ? process.MainWindowTitle
+                    : process.ProcessName;
+            }
+            catch
+            {
+                return "Unknown App";
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,21 +1,90 @@
-﻿using System.Windows;
+﻿using AudioNode.ViewModels;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 
 namespace AudioNode.Views
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ObservableCollection<AudioApp> ActiveApps { get; set; } = new ObservableCollection<AudioApp>();
+
+        // Store the master device so it doesn't get garbage collected
+        private MMDevice _defaultDevice;
+
+        // --- GLOBAL VOLUME PROPERTIES ---
+        public double GlobalVolume
+        {
+            get
+            {
+                if (_defaultDevice == null) return 0;
+                return _defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100;
+            }
+            set
+            {
+                if (_defaultDevice != null)
+                {
+                    // Update Windows Master Volume
+                    _defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = (float)(value / 100);
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(GlobalVolumeText));
+                }
+            }
+        }
+
+        public string GlobalVolumeText => $"{(int)GlobalVolume}%";
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            LoadAudioSessions();
+            AppListControl.ItemsSource = ActiveApps;
         }
 
+        private void LoadAudioSessions()
+        {
+            using var enumerator = new MMDeviceEnumerator();
+            // Get the Master Audio Device and save it to the class level variable
+            _defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            // LISTEN FOR OUTSIDE VOLUME CHANGES
+            _defaultDevice.AudioEndpointVolume.OnVolumeNotification += OnSystemVolumeChanged;
+
+            // Load Individual Apps
+            var sessionManager = _defaultDevice.AudioSessionManager;
+            for (int i = 0; i < sessionManager.Sessions.Count; i++)
+            {
+                var session = sessionManager.Sessions[i];
+                if (session.State == AudioSessionState.AudioSessionStateActive ||
+                    session.State == AudioSessionState.AudioSessionStateInactive)
+                {
+                    ActiveApps.Add(new AudioApp(session));
+                }
+            }
+        }
+
+        // Triggered when you use your keyboard/headset to change Windows volume
+        private void OnSystemVolumeChanged(AudioVolumeNotificationData data)
+        {
+            // Using the Dispatcher to update the UI thread, otherwise the app will crash
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                OnPropertyChanged(nameof(GlobalVolume));
+                OnPropertyChanged(nameof(GlobalVolumeText));
+            });
+        }
+
+        // --- WINDOW CONTROLS ---
         private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                this.DragMove();
-            }
+            if (e.LeftButton == MouseButtonState.Pressed) this.DragMove();
         }
 
         private void OnMinimizeClicked(object sender, RoutedEventArgs e)
@@ -25,7 +94,20 @@ namespace AudioNode.Views
 
         private void OnCloseClicked(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            // Clean up the event listener before closing to prevent memory leaks
+            if (_defaultDevice != null)
+            {
+                _defaultDevice.AudioEndpointVolume.OnVolumeNotification -= OnSystemVolumeChanged;
+                _defaultDevice.Dispose();
+            }
+            Application.Current.Shutdown();
+        }
+
+        // --- INOTIFY BOILERPLATE ---
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
