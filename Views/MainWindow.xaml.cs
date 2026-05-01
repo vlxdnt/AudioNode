8a415956b70fbdf8c3c71c2ec7b09fed442ca2f7
Vlad <r3ptil4@protonmail.com>
2026-05-01 18:26:23 +0300

    Updated the main window design

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AudioNode.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void OnMinimizeClicked(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void OnCloseClicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
