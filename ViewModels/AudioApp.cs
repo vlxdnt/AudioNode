0f996f2ffce66e59b2fe954f20159db266b3970a
Vlad <r3ptil4@protonmail.com>
2026-05-01 19:15:09 +0300

    Implemented audio slider design

---
+++ ViewModels/AudioApp.cs
@@ -0,0 +1,8 @@
+﻿namespace AudioNode.ViewModels
+{
+    public class AudioApp
+    {
+        public string AppName { get; set; } = string.Empty;
+        public double Volume { get; set; } // 0.0 to 100.0
+    }
+}
